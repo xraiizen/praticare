1b41230d57580d387dbfb90ec51a582135315c78
juliencln1s1 <juliencln1s1@gmail.com>
2023-12-01 10:48:24 +0100

    ?

--- frontend/praticare/lib/pages/SearchMapPage.dart
+++ frontend/praticare/lib/pages/SearchMapPage.dart
@@ -16,156 +16,181 @@ class _SearchMapPageState extends State<SearchMapPage> {
       Completer<GoogleMapController>();
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
+    target: LatLng(48.113759033957265, -1.6780924051285282),
+    zoom: 13,
   );
 
-  static const String customMapStyle = '''
-[
-  {
-    "featureType": "all",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {"weight": "2.00"}
-    ]
-  },
-  {
-    "featureType": "all",
-    "elementType": "geometry.stroke",
-    "stylers": [
-      {"color": "#9c9c9c"}
-    ]
-  },
-  {
-    "featureType": "all",
-    "elementType": "labels.text",
-    "stylers": [
-      {"visibility": "on"}
-    ]
-  },
-  {
-    "featureType": "landscape",
-    "elementType": "all",
-    "stylers": [
-      {"color": "#f2f2f2"}
-    ]
-  },
-  {
-    "featureType": "landscape",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {"color": "#ffffff"}
-    ]
-  },
-  {
-    "featureType": "landscape.man_made",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {"color": "#ffffff"}
-    ]
-  },
-  {
-    "featureType": "poi",
-    "elementType": "all",
-    "stylers": [
-      {"visibility": "off"}
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "all",
-    "stylers": [
-      {"saturation": -100},
-      {"lightness": 45}
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {"color": "#eeeeee"}
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {"color": "#7b7b7b"}
-    ]
-  },
-  {
-    "featureType": "road",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {"color": "#ffffff"}
-    ]
-  },
-  {
-    "featureType": "road.highway",
-    "elementType": "all",
-    "stylers": [
-      {"visibility": "simplified"}
-    ]
-  },
-  {
-    "featureType": "road.arterial",
-    "elementType": "labels.icon",
-    "stylers": [
-      {"visibility": "off"}
-    ]
-  },
-  {
-    "featureType": "transit",
-    "elementType": "all",
-    "stylers": [
-      {"visibility": "off"}
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "all",
-    "stylers": [
-      {"color": "#46bcec"},
-      {"visibility": "on"}
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "geometry.fill",
-    "stylers": [
-      {"color": "#c8d7d4"}
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.fill",
-    "stylers": [
-      {"color": "#070707"}
-    ]
-  },
-  {
-    "featureType": "water",
-    "elementType": "labels.text.stroke",
-    "stylers": [
-      {"color": "#ffffff"}
-    ]
+  // Définissez les coordonnées de votre marqueur
+  LatLng markerLatLng = LatLng(48.0900368054287, -1.6751246024528512);
+
+  // Liste pour stocker les marqueurs
+  List<Marker> markers = [];
+late BitmapDescriptor myIcon;
+  @override
+  void initState(){
+    super.initState();
+BitmapDescriptor.fromAssetImage(const ImageConfiguration(size: Size(48,48),) ,'assets/custom_marker.png').then((value) => value = myIcon);
   }
-]
-''';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ma Carte Google'),
+        title: Text('Map'),
       ),
       body: GoogleMap(
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
           controller.setMapStyle(customMapStyle);
+
+          // Ajoutez le marqueur à la liste
+          setState(() {
+            markers.add(
+              Marker(
+                markerId: MarkerId('mydigitalschool'),
+                position: markerLatLng,
+                infoWindow: InfoWindow(title: 'MyDigitalSchool'),
+                icon: myIcon,
+              ),
+            );
+          });
         },
+        markers: Set<Marker>.of(markers),
       ),
     );
   }
 }
+
+const String customMapStyle = '''
+    [
+      {
+        "featureType": "all",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {"weight": "2.00"}
+        ]
+      },
+      {
+        "featureType": "all",
+        "elementType": "geometry.stroke",
+        "stylers": [
+          {"color": "#9c9c9c"}
+        ]
+      },
+      {
+        "featureType": "all",
+        "elementType": "labels.text",
+        "stylers": [
+          {"visibility": "on"}
+        ]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+          {"color": "#f2f2f2"}
+        ]
+      },
+      {
+        "featureType": "landscape",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {"color": "#ffffff"}
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {"color": "#ffffff"}
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+          {"visibility": "off"}
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+          {"saturation": -100},
+          {"lightness": 45}
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {"color": "#eeeeee"}
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {"color": "#7b7b7b"}
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {"color": "#ffffff"}
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+          {"visibility": "simplified"}
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+          {"visibility": "off"}
+        ]
+      },
+      {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+          {"visibility": "off"}
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+          {"color": "#46bcec"},
+          {"visibility": "on"}
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+          {"color": "#c8d7d4"}
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {"color": "#070707"}
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {"color": "#ffffff"}
+        ]
+      }
+    ]
+    ''';
