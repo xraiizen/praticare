2bdcdf3e7880f4fb9ee60520882957cddc2ee38e
Etienne Morvan <etienne-morvan@hotmail.fr>
2023-12-01 10:57:32 +0100

    bug fix

--- frontend/praticare/lib/components/interface/BottomBar.dart
+++ frontend/praticare/lib/components/interface/BottomBar.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:praticare/components/BtnValidator.dart';
 
 class BottomBar extends StatefulWidget {
   final int selectedIndex;
@@ -14,13 +13,12 @@ class BottomBar extends StatefulWidget {
 
 class _BottomBarState extends State<BottomBar> {
   late int _selectedIndex;
-  late Widget? searchBtn;
+  late Widget searchBtn;
 
   @override
   void initState() {
     super.initState();
     _selectedIndex = widget.selectedIndex;
-    _updateSearchBtn();
   }
 
   void _goRoute(int index) {
@@ -38,53 +36,24 @@ class _BottomBarState extends State<BottomBar> {
     }
   }
 
-  void _updateSearchBtn() {
-    setState(() {
-      if (_selectedIndex == 0) {
-        searchBtn = SizedBox(
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: BtnValidator(
-              icon: Icons.search_sharp,
-              text: "Rechercher un praticien",
-              activePrimaryTheme: true,
-              routeName: "SearchMapPage",
-            ),
-          ),
-        );
-      } else {
-        searchBtn = null;
-      }
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Wrap(
-        children: [
-          searchBtn ?? const SizedBox.shrink(),
-          // Assuming NavigationBar is a custom widget you've created
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20.0),
-              topRight: Radius.circular(20.0),
-            ),
-            child: NavigationBar(
-              shadowColor: Colors.black,
-              animationDuration: const Duration(seconds: 1),
-              selectedIndex: _selectedIndex,
-              onDestinationSelected: (index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-                _goRoute(index);
-              },
-              destinations: _navBarItems,
-            ),
-          ),
-        ],
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(20.0),
+        topRight: Radius.circular(20.0),
+      ),
+      child: NavigationBar(
+        shadowColor: Colors.black,
+        animationDuration: const Duration(seconds: 1),
+        selectedIndex: _selectedIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          _goRoute(index);
+        },
+        destinations: _navBarItems,
       ),
     );
   }
